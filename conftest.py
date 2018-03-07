156a1fc79185ca9847314c91b15fd8cfed1dbad5
Ruben Homs <ruben.homs@wearespindle.com>
2018-03-07 16:40:23 +0100

    Remove salt function from conftest

--- conftest.py
+++ conftest.py
@@ -8,9 +8,3 @@ test_host = testinfra.get_host('paramiko://{KITCHEN_USERNAME}@{KITCHEN_HOSTNAME}
 @pytest.fixture
 def host():
     return test_host
-
-@pytest.fixture
-def salt():
-    test_host.run('sudo chown -R {0} /tmp/kitchen'.format(os.environ.get('KITCHEN_USERNAME')))
-    tmpconf = '/tmp/kitchen/etc/salt'
-    return functools.partial(test_host.salt, config=tmpconf)
